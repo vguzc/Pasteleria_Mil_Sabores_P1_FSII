f44bec2231f2492d66c6bcc60cf9d12466b91dbf
guillermo-manriquez <gu.manriquez@duocuc.cl>
2026-09-10 00:22:38 -0300

    feat(registro): agrega array de regiones y comunas de Chile(4)

---
+++ js/regiones-comunas.js
@@ -0,0 +1,18 @@
+const regionesYComunas = [
+    {
+        region: "Región Metropolitana de Santiago",
+        comunas: ["Santiago", "Providencia", "Las Condes", "Ñuñoa", "Maipú", "La Florida", "Macul", "Puente Alto"]
+    },
+    {
+        region: "Región de Valparaíso",
+        comunas: ["Valparaíso", "Viña del Mar", "Concón", "Quilpué", "Villa Alemana"]
+    },
+    {
+        region: "Región del Biobío",
+        comunas: ["Concepción", "Talcahuano", "San Pedro de la Paz", "Chiguayante", "Los Ángeles"]
+    },
+    {
+        region: "Región de La Araucanía",
+        comunas: ["Temuco", "Padre Las Casas", "Villarrica", "Pucón"]
+    }
+];
